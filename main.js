ea7a7ef1a9d1fee43c0a9f2be7545b70a2f2f647
ariana-ssilva <arianamoreira5021@gmail.com>
2022-07-25 19:53:32 -0300

    update menu responsivo

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+const menu = document.querySelector('nav .container ul');
+
+// show menu
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+// hide menu
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+
+
 // skills show-hide
 const skillItems = document.querySelectorAll('section.skills .skill');
 
@@ -18,24 +40,6 @@ const swiper = new Swiper('.swiper', {
 
 
 // menu sidebar
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-
-const menu = document.querySelector('nav .container ul');
-
-// show menu
-menuBtn.addEventListener('click', () => {
-    menu.style.display = 'block';
-    menuBtn.style.display = 'none';
-    closeBtn.style.display = 'inline-block';
-})
-
-// hide menu
-closeBtn.addEventListener('click', () => {
-    menu.style.display = 'none';
-    menuBtn.style.display = 'inline-block';
-    closeBtn.style.display = 'none';
-})
 
 const navItems = menu.querySelectorAll('li');
 
